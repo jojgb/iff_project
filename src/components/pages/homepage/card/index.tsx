6fb34186cb26f1fbbe22bd1dbda9be28bbb1687b
Jonathan <jojgb96@gmail.com>
2025-01-07 23:36:12 +0700

    home page done

--- src/components/pages/homepage/card/index.tsx
+++ src/components/pages/homepage/card/index.tsx
@@ -6,15 +6,37 @@ import merchandiseImage from "../../../../assets/merchandise.png";
 
 interface CardProps {
   className?: string;
+  onClick?: (category: string) => void;
 }
 
-const Card: FunctionComponent<CardProps> = () => {
+const Card: FunctionComponent<CardProps> = ({ onClick }) => {
+  const handleClick = (category: string) => {
+    if (onClick) {
+      onClick(category);
+    }
+  };
   return (
     <div className="flex gap-4">
-      <img src={periperalImage} alt="periperal" />
-      <img src={printingImage} alt="printing" />
-      <img src={stationaryImage} alt="stationary" />
-      <img src={merchandiseImage} alt="merchandise" />
+      <img
+        src={periperalImage}
+        alt="periperal"
+        onClick={() => handleClick("Peripheral")}
+      />
+      <img
+        src={printingImage}
+        alt="printing"
+        onClick={() => handleClick("Printing")}
+      />
+      <img
+        src={stationaryImage}
+        alt="stationary"
+        onClick={() => handleClick("Stationary")}
+      />
+      <img
+        src={merchandiseImage}
+        alt="merchandise"
+        onClick={() => handleClick("Merchandise")}
+      />
     </div>
   );
 };
--- src/components/pages/homepage/index.tsx
+++ src/components/pages/homepage/index.tsx
@@ -1,11 +1,18 @@
-import { FunctionComponent } from "react";
+import { FunctionComponent, useState } from "react";
 import styles from "./homePage.module.scss";
 import Card from "./card";
+
 interface HomePageProps {
   className?: string;
 }
 
 const HomePage: FunctionComponent<HomePageProps> = () => {
+  const [needSelected, setNeedSelected] = useState<string>("");
+  const handleCategoryClick = (category: string) => {
+    setNeedSelected(category);
+    console.log(`Selected category: ${category}`);
+  };
+
   return (
     <div>
       {/* tab section  */}
@@ -20,15 +27,23 @@ const HomePage: FunctionComponent<HomePageProps> = () => {
       <section className={styles.midSection}>
         <div>
           <h1>E-Catalogue</h1>
-          <h1>select what you need here</h1>
-          <p className="mt-4">
-            Select your needs based on the categories below
-          </p>
+          {needSelected ? (
+            <div>
+              <h1>{needSelected}</h1>
+            </div>
+          ) : (
+            <div>
+              <h1>Select what you need here</h1>
+              <p className="mt-4">
+                Select your needs based on the categories below
+              </p>
+            </div>
+          )}
         </div>
       </section>
       {/* card section  */}
       <section className={styles.cardSection}>
-        <Card />
+        <Card onClick={handleCategoryClick} />
       </section>
     </div>
   );
